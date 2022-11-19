ca73bb5fb5ac1ef086d959929082b60ec065e0d8
Rizwan <eminence.rizwan@gmail.com>
2022-11-19 18:51:26 +0530

    side bar update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
-import Sidebar from "./Component/common/Layout/Sidebar";
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+ import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function App() {
+import SideBar from "./Component/Sidebar/SideBar";
+import MainRoutes from "./Router/Routes";
+import "./App.css";
+
+const App = () => {
+  const [sidebarIsOpen, setSidebarOpen] = useState(true);
+  const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
+
   return (
-    <div>
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="App wrapper">
+        <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
+        <MainRoutes toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
+      </div>
+    </Router>
   );
-}
+};
+
+export default App;
